0b37a6061934dd4af35c2e9a4dc412ecc92b22bb
Nousquantech <143766008+Nousquantech@users.noreply.github.com>
2025-01-09 12:03:03 -0500

    SQL Cleaning Data
    SQL cleaning data project with MySql

---
+++ SQL_Cleaning_Data_MySQL.sql
@@ -0,0 +1,156 @@
+-- Data Cleaning
+
+SELECT *
+FROM layoffs;
+
+-- 1. Remove Duplicates
+-- 2. Standardize the Data
+-- 3. Null Values or Blank Values
+-- 4. Remove Any Columns
+
+-- Copy real database to workdatabase
+CREATE TABLE layoffs_staging
+LIKE layoffs;
+-- Check
+SELECT *
+FROM layoffs_staging;
+
+-- Insert Data
+
+INSERT layoffs_staging
+SELECT *
+FROM layoffs;
+
+-- 1. Remove Duplicates
+SELECT *,
+ROW_NUMBER() OVER(PARTITION BY company,industry,total_laid_off,percentage_laid_off,`date`) AS row_num
+FROM layoffs_staging;
+
+WITH duplicate_cte AS
+(
+SELECT *,
+ROW_NUMBER() OVER(PARTITION BY company,location,industry,total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions) AS row_num
+FROM layoffs_staging
+)
+SELECT *
+FROM duplicate_cte
+WHERE row_num >1;
+
+
+
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` INT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+SELECT *
+FROM layoffs_staging2;
+
+INSERT INTO layoffs_staging2
+SELECT *,
+ROW_NUMBER() OVER(PARTITION BY company,location,industry,total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions) AS row_num
+FROM layoffs_staging;
+
+SELECT *
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+DELETE 
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+SELECT *
+FROM layoffs_staging2;
+
+-- 2. Standardize the Data
+
+-- trim company name
+SELECT company, TRIM(company)
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET company = TRIM(company);
+
+-- standardize industry
+SELECT DISTINCT industry
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET industry = 'Crypto'
+WHERE industry LIKE 'Crypto%';
+
+-- standardize country
+SELECT *
+FROM layoffs_staging2
+WHERE country LIKE 'United States%';
+
+UPDATE layoffs_staging2
+SET country  = TRIM(TRAILING '.' FROM country)
+WHERE country LIKE 'United States%';
+
+-- standardize date convert to date format
+SELECT `date`,
+STR_TO_DATE(`date`,'%m/%d/%Y')
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET `date` = STR_TO_DATE(`date`,'%m/%d/%Y');
+
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN `date` DATE;
+
+-- 3. Null Values or Blank Values
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL
+OR industry = '';
+
+UPDATE layoffs_staging2
+SET industry = NULL
+WHERE industry = '';
+
+
+SELECT *
+FROM layoffs_staging2
+WHERE company LIKE 'Bally%';
+
+SELECT t1.industry,t2.industry
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company    
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL;
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company    
+SET t1.industry = t2.industry
+WHERE t1.industry IS NULL
+AND t2.industry IS NOT NULL;
+
+SELECT *
+FROM layoffs_staging2;
+
+-- delete rows with null total_laid-off and percentage_laid_off
+DELETE
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num
